7711db111d0a2ef22c255ed15f3f7712d3aeae07
Matias Honaine <honainematias@gmail.com>
2024-07-02 19:22:53 -0300

    first commit

---
+++ desafio7/src/helpers/helper.js
@@ -0,0 +1,13 @@
+const helper = {
+    jsonStringify(object) {
+        if (object == undefined) {
+            return `null`;
+        }
+        return JSON.stringify(object);
+    },
+    assignAppbar(user) {
+        return user.rol === "admin" ? 'appbar_admin' : 'appbar';
+    }
+};
+
+module.exports = helper;
---
+++ desafio7/src/utils/utils.js
@@ -0,0 +1,26 @@
+function handleQueryString(queryObject, value) {
+    if (value) {
+        const queryArr = Object.keys(queryObject).map((e) =>
+            e == "page" ? `${e}=${value}` : `${e}=${queryObject[e]}`
+        );
+        if (!queryObject.page) {
+            queryArr.push(`page=${value}`);
+        }
+        return "/views/products?" + queryArr.join("&");
+    }
+    return null;
+}
+
+function checkNewProduct(product) {
+    const fields = ["title", "description", "price", "code", "stock", "category"];
+    if (
+        fields.every((e) => {
+            return !!product[e];
+        })
+    ) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = { handleQueryString, checkNewProduct };
